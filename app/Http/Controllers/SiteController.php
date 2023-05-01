743644a0a5d68ecc324f564f16ff0804961cbd7c
Giordani da Silveira dos Santos <dani.santossilveira@hotmail.com>
2023-05-01 02:59:41 -0300

    ajuste

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -13,7 +13,7 @@ class SiteController extends Controller
         $banners = Banner::orderBy('ordem','desc')->where('tipo_posicao', 'cap')->where('ativo', 1)->get()->all();
         $imagens = Foto::orderBy('data_publicacao','desc')->where('destaque', 1)->get()->take(5);
         $video = Video::orderBy('data_publicacao','desc')->where('destaque', 1)->get()->take(1)->first();
-        $devocional = Video::orderBy('data_publicacao','desc')->where('destaque', 1)->where('devocional', 1)->get()->take(1)->first();
+        $devocional = Video::orderBy('data_publicacao','desc')->where('devocional', 1)->get()->take(1)->first();
        
         return view('site.principal.index', [
             'banners'=>$banners,
